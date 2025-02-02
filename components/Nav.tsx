cda7e6a3d721bb51473051a9997d974fe71afcf9
McFly <matthieu.roman2002@gmail.com>
2025-02-02 17:40:57 +0100

    frst

---
+++ components/Nav.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { RainbowButton } from "@/components/ui/rainbow-button";
+import { ShimmerButtonNav } from "@/components/ui/shimmer-button-nav";
+
+export default function Nav() {
+  return (
+    <nav className="flex justify-between items-center h-[58px]">
+      <ShimmerButtonNav className="px-4 py-1 shadow-2xl flex items-center justify-center gap-[10px] border border-[#FDFDFD59] rounded">
+        <Image
+          className="rounded-full"
+          src="/pp.png"
+          width={43}
+          height={43}
+          alt="Jeremy Coron"
+        />
+        <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+          Jeremy Coron 🧠
+        </span>
+      </ShimmerButtonNav>
+      <ShimmerButtonNav className="shadow-2xl flex items-center justify-center gap-[40px] border border-[#FDFDFD59] rounded">
+        <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+          Module de programme
+        </span>
+        <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+          Résultats des coachés
+        </span>
+        <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+          Prix
+        </span>
+      </ShimmerButtonNav>
+      <div>
+        <RainbowButton>Apprends à maîtriser ton stress</RainbowButton>
+      </div>
+    </nav>
+  );
+}
